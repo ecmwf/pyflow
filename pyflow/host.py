623e48ef4bf3bba0914373a5f168ceb62887d4c2
Corentin Carton De Wiart <corentin.carton@ecmwf.int>
2023-11-09 15:12:54 +0000

    bugfix in trap script

--- pyflow/host.py
+++ pyflow/host.py
@@ -336,7 +336,7 @@ class Host:
             export SIGNAL_LIST='1 2 3 4 5 6 7 8 10 11 13 24 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64'
 
             for signal in $SIGNAL_LIST; do
-                trap "ERROR $signal \"Signal $(kill -l $signal) ($signal) received \"" $signal
+                trap "ERROR $signal \\"Signal $(kill -l $signal) ($signal) received \\"" $signal
             done
 
             # Trap any calls to exit and errors caught by the -e flag
