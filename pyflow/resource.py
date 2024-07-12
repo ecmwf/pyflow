3b603b518801b91c93cb5b7acbe0e1b821e08283
Tobias Kremer <tobias.kremer@ecmwf.int>
2024-07-12 11:43:47 +0200

    Added serverfilename for data resource

--- pyflow/resource.py
+++ pyflow/resource.py
@@ -106,7 +106,7 @@ class Resource(Task):
         for h in self._hosts:
             lines += h.copy_file_to(self._server_filename, self.location()).split("\n")
 
-        return lines, []
+        return lines, [] 
 
     def location(self):
         """
@@ -160,6 +160,8 @@ class DataResource(Resource):
             target(Deployment): The deployment target.
             filename(str): The filename for the resource data.
         """
+
+        self._server_filename = filename
 
         """
         Resources don't all need to save data at generation time
